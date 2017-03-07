8ee368883c6ad24f8c9797b0f3db831c77da97ab
Sebastian Szaniszlo <Sebastian Szaniszlo>
2017-03-07 22:39:10 +0000

    Removed unnecessary comments

--- app.js
+++ app.js
@@ -29,7 +29,6 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
 //Body parser MW
-//app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 //Passport MW
