f29fb5e54d18fc98b4ba468323e1a9d6117f861f
Sebastian Szaniszlo <sebastian.szaniszlo@gmail.com>
2019-09-27 14:20:35 +0100

    Moved password hashing into its own service

--- src/services/user-creator.ts
+++ src/services/user-creator.ts
@@ -1,17 +1,10 @@
-import { genSalt, hash } from 'bcryptjs';
 import { IUser } from '../models/user';
+import passwordHasher from './password-hasher';
 
 class UserCreator {
   public createUser(user: IUser, callback: (err: any, res: IUser) => void) {
-    genSalt(10, (err: Error, salt: string) => {
-      hash(user.password, salt, (err: Error, hash: string) => {
-        if (err) {
-          throw err;
-        }
-        user.password = hash;
-        user.save(callback);
-      });
-    });
+    user.password = passwordHasher.hashPassword(user.password);
+    user.save(callback);
   }
 }
 
